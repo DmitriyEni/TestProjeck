b047cd660cafb98b317773e7137a86eeb282c30a
DmitriyEni <eni.dmitriy@mail.ru>
2022-05-05 19:34:41 +0300

    Create Project

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Dmitriy Eni on 05.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
